39a204004c8ab7e2b99b2adedacefcedca422c3a
w453908766 <w453908766@126.com>
2024-06-17 11:46:06 +0800

    fix

--- qdebug.js
+++ qdebug.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const process = require('process')
 const { spawn } = require('child_process');
 
-let debugPath = path.join(os.tmpdir(), "newDebug.quickdebug")
+let debugPath = ".newDebug.quickdebug"
+let program = path.resolve(process.argv[2])
 
 let launch = {
   type: "lldb",
   name: "newDebug",
   request: "launch",
-  program: process.argv[2],
+  program: program,
   args: process.argv.slice(3),
   cwd: process.cwd(),
   env: process.env
--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,10 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.workspace.onDidOpenTextDocument((e: vscode.TextDocument) => {
       if(e.fileName.endsWith('.quickdebug')){
         let info = JSON.parse(e.getText())
-        vscode.debug.startDebugging(undefined, info)
-
         let tab = vscode.window.tabGroups.activeTabGroup.activeTab
         if(tab) vscode.window.tabGroups.close(tab, true)
+
+        vscode.debug.startDebugging(undefined, info)
       }
     })
   )
