ae17ad6c32d4ffc74f61abda9ae52adf729159fc
w453908766 <w453908766@126.com>
2024-07-21 21:23:56 +0800

    add --dir option

--- qdebug.js
+++ qdebug.js
@@ -5,19 +5,37 @@ const path = require('path')
 const process = require('process')
 const { spawn } = require('child_process');
 
-let debugPath = ".newDebug.quickdebug"
-let program = path.resolve(process.argv[2])
+function parseArgv(argv){
+  let index = 2
+  let dir = ''
+  while(index < argv.length){
+    if(!argv[index].startsWith('--')) {
+      break
+    } else if(argv[index] == '--dir'){
+      dir = argv[index+1]
+      index = index + 2
+    } else {
+      index = index + 1
+    }
+  }
+  let output = path.join(dir, ".newDebug.quickdebug")
+  let program = path.resolve(process.argv[index])
+  let args = process.argv.slice(index + 1)
+  return { output, program, args }
+}
+
+let option = parseArgv(process.argv)
 
 let launch = {
   type: "lldb",
   name: "newDebug",
   request: "launch",
-  program: program,
-  args: process.argv.slice(3),
+  program: option.program,
+  args: option.args,
   cwd: process.cwd(),
   env: process.env
 }
 
-fs.writeFileSync(debugPath, JSON.stringify(launch));
+fs.writeFileSync(option.output, JSON.stringify(launch));
 
-spawn('code', [debugPath]);
+spawn('code', [option.output]);
